bdf08ba118742b98ac8483951396bd79de37f66c
KatarinaFranekova <katarinafranekova22@gmail.com>
2023-02-19 12:31:34 +0100

    domceky - cyklus, prasiatko s dvoma nozickami

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -2,10 +2,73 @@ package cz.czechitas.java.lekce02;
 
 import cz.czechitas.java.lekce02.engine.Turtle;
 
-public class HlavniProgram {
+public class
+HlavniProgram {
     private Turtle zofka;
     public void start() {
         zofka = new Turtle();
+
+
+        for (int x = 0; x < 3; x++) {
+
+            for (int i = 0; i < 4; i++) {
+                zofka.move(100);
+                zofka.turnRight(90);
+            }
+
+            zofka.penUp();
+            zofka.move(100);
+
+            zofka.turnRight(30);
+            zofka.penDown();
+            zofka.move(100);
+            zofka.turnRight(120);
+            zofka.move(100);
+
+            zofka.turnRight(30);
+            zofka.penUp();
+            zofka.move(100);
+
+            zofka.turnLeft(90);
+            zofka.penUp();
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.penDown();
+        }
+
+     /*  zofka.turnLeft(60);
+        zofka.move(100);
+        zofka.turnRight(120);
+        zofka.move(100);
+
+        zofka.turnRight(120);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.penDown();
+
+        zofka.turnRight(30);
+        zofka.move(50);
+        zofka.penUp();
+        zofka.turnLeft(120);
+        zofka.move(50);
+        zofka.turnLeft(120);
+        zofka.penDown();
+        zofka.move(50);
+
+        zofka.turnRight(120);
+        zofka.penUp();
+        zofka.move(100);
+
+        zofka.turnRight(60);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.penUp();
+        zofka.turnRight(120);
+        zofka.move(50);
+        zofka.turnRight(120);
+        zofka.penDown();
+        zofka.move(50);*/
+
 
         //TODO tady bude kód
     }
